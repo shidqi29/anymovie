07d905c168e70badef38f366ccee6275ede2ee1e
shidqi29 <shidqiamirulhaq@gmail.com>
2023-08-05 12:46:46 +0700

    initialize project with vite and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen w-full flex justify-center items-center bg-slate-500 pt-2 p-4">
+      hello world!
+    </div>
+  );
+}
+
+export default App;
