0aaf3c0dd266b96a865f650a396690978769275d
shidqi29 <shidqiamirulhaq@gmail.com>
2023-08-07 10:17:15 +0700

    feat[homePage] : add loading element

---
+++ src/components/elements/loading/Loading.jsx
@@ -0,0 +1,7 @@
+const LoadingSpinner = () => {
+  return (
+    <div className="w-12 h-12 rounded-full animate-spin border-8 border-gray-600 border-t-8 border-t-gray-700 flex items-center justify-center"></div>
+  );
+};
+
+export default LoadingSpinner;
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from "react";
 import { movieList } from "../services/movieList.service";
 import MovieList from "../components/fragments/MovieList";
+import LoadingSpinner from "../components/elements/loading/Loading";
 
 const HomePage = () => {
   const [movies, setMovies] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   const getMovies = async () => {
     try {
       const data = await movieList();
       setMovies(data.results);
+      setIsLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -17,30 +20,34 @@ const HomePage = () => {
     getMovies();
   }, []);
 
-  if (!movies) return "Loading";
-
   return (
     <>
-      <div className="w-full flex flex-wrap ">
-        {movies.length > 0 &&
-          movies.map((movie) => (
-            <MovieList key={movie.id}>
-              <MovieList.Header
-                title={movie.title}
-                release_date={movie.release_date}
-              />
-              <MovieList.Body
-                image={
-                  movie.poster_path !== null
-                    ? `https://image.tmdb.org/t/p/w400/${movie.poster_path}`
-                    : "https://via.placeholder.com/400"
-                }
-                title={movie.title}>
-                {movie.overview}
-              </MovieList.Body>
-            </MovieList>
-          ))}
-      </div>
+      {isLoading ? (
+        <div className="flex items-center justify-center min-h-screen">
+          <LoadingSpinner />
+        </div>
+      ) : (
+        <div className="w-full flex flex-wrap ">
+          {movies.length > 0 &&
+            movies.map((movie) => (
+              <MovieList key={movie.id}>
+                <MovieList.Header
+                  title={movie.title}
+                  release_date={movie.release_date}
+                />
+                <MovieList.Body
+                  image={
+                    movie.poster_path !== null
+                      ? `https://image.tmdb.org/t/p/w400/${movie.poster_path}`
+                      : "https://via.placeholder.com/400"
+                  }
+                  title={movie.title}>
+                  {movie.overview}
+                </MovieList.Body>
+              </MovieList>
+            ))}
+        </div>
+      )}
     </>
   );
 };
