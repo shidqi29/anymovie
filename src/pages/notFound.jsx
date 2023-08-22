00df7a2670de61f73bc09cb631bd44a0384fc926
shidqi29 <shidqiamirulhaq@gmail.com>
2023-08-22 10:40:15 +0700

    refactor[notFound]: change button to link

--- src/pages/notFound.jsx
+++ src/pages/notFound.jsx
@@ -1,18 +1,14 @@
-import Button from "../components/elements/button/Button";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen text-secondary px-5">
       <h1 className="font-bold text-7xl mb-5 text-accent">Oops!</h1>
       <p className="mb-20 text-center">{`Sorry, we cannot find the page you are looking for :(`}</p>
-
-      <Button
-        classname=" p-2 border border-accent hover:bg-accent hover:text-primary transition-all"
-        onClick={() => {
-          window.location.href = "/";
-        }}>
-        Back to Main Page
-      </Button>
+      <Link to="/" className="p-2 rounded-md border border-accent hover:bg-accent hover:text-primary transition-all">
+        {" "}
+        Back to main page
+      </Link>
     </div>
   );
 };
