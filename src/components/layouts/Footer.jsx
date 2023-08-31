85dc5f1f4dde96b692b8d58919c6792514bef9c5
shidqi29 <shidqiamirulhaq@gmail.com>
2023-08-31 13:09:06 +0700

    style[footer]: change footer

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -3,17 +3,8 @@ const Footer = () => {
     <footer className="px-6 text-sm bg-accent">
       <div className="px-3 md:px-0 text-center md:text-left text-primary flex items-center py-4 md:py-8 ">
         <p>
-          Built by <span className="font-semibold">Shidqi</span>. This project
-          is a clone of website {""}
-          <a
-            href="https://pahe.li"
-            target="_blank"
-            className="underline"
-            rel="noreferrer">
-            pahe.li
-          </a>
-          {". "}
-          The source code is available on{" "}
+          Built with ❤ by <span className="font-semibold">Shidqi</span>. The
+          source code is available on{" "}
           <a
             href="https://github.com/shidqi29/anymovie"
             target="_blank"
